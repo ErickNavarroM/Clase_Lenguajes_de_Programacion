8d9f72bdb64648e6a193d424c398bd1083313c29
ErickNavarroM <name020609@gmail.com>
2022-08-26 08:50:16 -0700

    coin

--- tarea-02/arithlang.rkt
+++ tarea-02/arithlang.rkt
@@ -1,14 +1,41 @@
 #lang plait
 
 ;; Problema 1
-(define-type Expr
-  (num [n : Number])
-  (plusC [m : Expr][n : Expr])
-  (multC [m : Expr][n : Expr]))
-
 (define-type ArithC
-  ()
-  
+  (numC [n : Number])
+  (plusC [p : Expr] [q : Expr])
+  (multC [p : Expr] [q : Expr])
+  (condC [p : Expr] [q : Expr] [r : Expr]))
+
+(define-type ArithS
+  (numS [n : Number])
+  (plusS [p : Expr] [q : Expr])
+  (multS [p : Expr] [q : Expr])
+  (bminusS [p : Expr] [q : Expr])
+  (uminusS [p : Expr])
+  (negS [p : Expr])
+  (disjS [p : Expr] [q : Expr])
+  (conjS [p : Expr] [q : Expr])
+  (condS [p : Expr] [q : Expr] [r : Expr]))
+
+(define (interp [c : ArithC]) : Number
+  (type-case ArithC c
+    [(numC n) n]
+    [(plusC p q) (+ p q)]
+    [(multC p q) (* p q)]
+    [(condC p q r) (if p q r)]))
+
+(define (desugar [s : ArithS]) : ArithC
+  (type-case ArithS s
+    [(numS n) (numC n)]
+    [(plusS p q) (plusC p q)]
+    [(multS p q) (multC p q)]
+    [(bminusS p q) (plusC p (uminusS q))]
+    [(uminusS p) (plusC p )]
+    [(negS p) (condC p #f #t)]
+    [(disjS p q) (condC p #t )]
+    [(conjS p q) (condC )]
+    [(condS p q r) (condC p q r)]))
 
 (define (parse [s : S-Exp]) : ArithS
   (cond [(s-exp-number? s) (numC (s-exp->number s))]
@@ -25,8 +52,6 @@
              [(if) (condS (parse (second ls)) (parse (third ls)) (parse (fourth ls)))]
              [else (error 'parse ”operación aritmética malformada”)]))]
         [else (error 'parse ”expresión aritmética malformada”)]))
-
-(define (desugar) 5)
 
 (define (interp) 6)
 
